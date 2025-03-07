8012331559e5438e20596014d5bb966b557f23c7
unknown <veddwarka@gmail.comm>
2025-03-07 14:11:39 +0000

    Final exercise

---
+++ Final_Exercise.R
@@ -0,0 +1,31 @@
+data(airquality)
+head(airquality)
+summary(airquality)
+str(airquality)
+
+#airquality dataset was chosen so that I could also perform some data cleaning
+
+
+#Part3
+#We need to find the possible data errors in the dataset and try to either 
+#replace them or omit them.
+
+install.packages("ggplot2","tidyverse","dplyr")
+library(ggplot2,tidyverse ,dplyr)
+
+
+is.na(airquality)
+airquality<- airquality %>% 
+  dplyr::rename(Temperature=Temp)
+
+
+clean_data<- na.omit(airquality)
+
+summary(clean_data)
+
+#Part4: Graphs
+
+data(clean_data)
+library(ggplot2)
+ggplot(clean_data, aes(x=Ozone,y=Temperature)+geom_point())
+
